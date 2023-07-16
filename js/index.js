c7fecc16d65522d79e432a56b9d88f42171ffa63
Karan Gupta <77609042+Karan958@users.noreply.github.com>
2023-07-16 11:22:13 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 
 
 let inputDir = { x: 0, y: 0 };
-const foodSound = new Audio("https://github.com/Karan958/Snakes-Game/blob/master/music/food.mp3");
-const gameOverSound = new Audio("https://github.com/Karan958/Snakes-Game/blob/master/music/gameover.mp3");
-const musicSound = new Audio("https://github.com/Karan958/Snakes-Game/blob/master/music/music.mp3");
-const moveSound = new Audio("https://github.com/Karan958/Snakes-Game/blob/master/music/move.mp3");
+const foodSound = new Audio("../music/food.mp3");
+const gameOverSound = new Audio("../music/gameover.mp3");
+const musicSound = new Audio("../music/music.mp3");
+const moveSound = new Audio("../music/move.mp3");
 let speed = 7;
 let score = 0;
 let lastPaintTime = 0;
